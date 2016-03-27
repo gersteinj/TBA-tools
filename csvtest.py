41da00f7c4b1980d8211c52d2525c9cb7e0f4534
gersteinj <gersteinj@gmail.com>
2016-03-27 15:55:38 -0400

    can write dict to CSV

--- csvtest.py
+++ csvtest.py
@@ -1,8 +1,8 @@
 import csv
-items = []
-for n in range(15):
-    items.append(n)
+my_dict = {1257: 9, 1626: 5, 1676: 6, 5624: 1}
+
 
 with open('writetest.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(items)
+    for key, value in my_dict.items():
+        writer.writerow([key, value])
