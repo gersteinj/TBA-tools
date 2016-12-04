374f0b2153ac13faaaee1ef2737df337b8093192
gersteinj <gersteinj@gmail.com>
2016-12-04 15:25:19 -0500

    can dump to json

--- tbat2.py
+++ tbat2.py
@@ -1,4 +1,5 @@
 import tbat
+import json
 
 
 class Team(object):
@@ -16,11 +17,13 @@ class Team(object):
         self.rookie_year = results['rookie_year']
         self.motto = results['motto']
         self.key = results['key']
-        # self.years_active = tbat.get_team_years_participated(results['team_number'])
+        self.years_active = tbat.get_team_years_participated(results['team_number'])
+        self.event_history = tbat.get_team_event_history(results['team_number'])
+        self.award_history = tbat.get_team_awards_history(results['team_number'])
 
 
 # Do this with a dictionary instead?
-def fetch_team_info(test=False):
+def update_team_info(test=False):
     """Fetches all team info. Do only as needed."""
     # Get all teams
     if(test):
@@ -31,7 +34,10 @@ def fetch_team_info(test=False):
     # Create objects
     teams = {}
     for team in all_teams:
-        teams[team['team_number']] = Team(team)
+        temp_team = Team(team)
+        teams[team['team_number']] = json.dumps(vars(temp_team), indent=4, sort_keys=True)
+    with open('all_teams.json', 'w') as all_teams:
+        json.dump(teams, all_teams, indent=4, sort_keys=True)
     return teams
 
 
@@ -39,3 +45,4 @@ def fetch_team_info(test=False):
 # teams = fetch_team_info()
 # for team in teams:
 #     print("Team %s is from %s. They are typically called %s. They were active in %s" % (team.team_number, team.locality, team.nickname, team.years_active))
+update_team_info(test=True)
